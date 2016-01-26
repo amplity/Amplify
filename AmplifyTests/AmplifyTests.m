0cd98cccfae964808b5f38011a8cd6f401a8a581
ericxu <xuhui@harmay.com>
2016-01-26 11:11:28 +0800

    create

---
+++ AmplifyTests/AmplifyTests.m
@@ -0,0 +1,39 @@
+//
+//  AmplifyTests.m
+//  AmplifyTests
+//
+//  Created by ZhangJixu on 16/1/25.
+//  Copyright © 2016年 hm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AmplifyTests : XCTestCase
+
+@end
+
+@implementation AmplifyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,20 @@
+
+pod 'MBProgressHUD', '~> 0.9.2'
+
+pod 'FMDB'
+# pod 'FMDB/FTS'   # FMDB with FTS
+# pod 'FMDB/standalone'   # FMDB with latest SQLite amalgamation source
+# pod 'FMDB/standalone/FTS'   # FMDB with latest SQLite amalgamation source and FTS
+# pod 'FMDB/SQLCipher'   # FMDB with SQLCipher
+pod 'TTTAttributedLabel'
+
+pod 'MJRefresh'
+pod 'MJExtension'
+
+platform :ios, '6.1'
+pod 'SDWebImage', '~>3.7'
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+pod 'AFNetworking', '~> 3.0'
